25d6c672747e10e30e9b99473292ad9fdb8f7417
Ruslan Kasyanovich <ruslank906@gmail.com>
2020-12-20 16:46:45 +0200

    Додано підрахунок суми чисел і виведення результату

--- Program.cs
+++ Program.cs
@@ -10,6 +10,10 @@ namespace GitLesson
     {
         static void Main(string[] args)
         {
+            int x = 5;
+            int y = 10;
+            Console.WriteLine("5+10 = {0}", x  + y);
+            Console.ReadKey();
         }
     }
 }
